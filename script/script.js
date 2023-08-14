3310ed15a465d86f9fe45741594e6722b7dce32a
ritikam1234 <ritikam@andrew.cmu.edu>
2023-08-14 21:20:39 +0800

    about section implemented

---
+++ script/script.js
@@ -0,0 +1,8 @@
+
+/* Typed JS for intorduction animtion*/
+import Typed from "https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js";
+
+var typed = new Typed('#element', {
+    strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+    typeSpeed: 50,
+});
